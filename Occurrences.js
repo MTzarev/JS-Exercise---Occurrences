3da1b84efd88e0dfddaa2bab4ea1b531a07384bf
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-22 21:27:57 +0300

    Update Occurrences.js

--- Occurrences.js
+++ Occurrences.js
@@ -6,9 +6,7 @@ function countStringOccurrences(text, searched) {
             count++;
         }
     }
-
     console.log(count);
-
 }
 countStringOccurrences('This is a word and it also is a sentence',
 
